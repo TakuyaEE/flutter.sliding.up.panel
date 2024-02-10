d48eedfc8a267f37e047ae271259652d5fe1dc1a
TakuyaIi <takuyaelle@gmail.com>
2024-02-10 15:17:01 +0900

    ✨ DefaultTabControllerでのタブ実装まで

---
+++ sliding_up_panel_test/lib/__main.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+void main() => runApp(const SlidingUpPanelExample());
+
+class SlidingUpPanelExample extends StatelessWidget {
+  const SlidingUpPanelExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.grey[200],
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarDividerColor: Colors.black,
+    ));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SlidingUpPanel Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SlidingUpPanelExample"),
+      ),
+      body: SlidingUpPanel(
+        panel: const Center(
+          child: Text("This is the sliding Widget"),
+        ),
+        body: const Center(
+          child: Text("This is the Widget behind the sliding panel"),
+        ),
+      ),
+    );
+  }
+}
--- sliding_up_panel_test/lib/main.dart
+++ sliding_up_panel_test/lib/main.dart
@@ -1,125 +1,275 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const SlidingUpPanelExample());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SlidingUpPanelExample extends StatelessWidget {
+  const SlidingUpPanelExample({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.grey[200],
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarDividerColor: Colors.black,
+    ));
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'SlidingUpPanel Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  final double _initFabHeight = 120.0;
+  double _fabHeight = 0;
+  double _panelHeightOpen = 0;
+  final double _panelHeightClosed = 95.0;
+  late TabController? _tabController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _fabHeight = _initFabHeight;
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController!.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_tabController != null) {
+      _tabController!.dispose();
+    }
+    super.dispose();
+  }
+
+  _handleTabSelection() {
+    if (_tabController != null) {
+      if (_tabController!.indexIsChanging) {
+        setState(() {});
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    _panelHeightOpen = MediaQuery.of(context).size.height * .80;
+
+    return Material(
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          SlidingUpPanel(
+            maxHeight: _panelHeightOpen,
+            minHeight: _panelHeightClosed,
+            parallaxEnabled: true,
+            parallaxOffset: .5,
+            body: _body(),
+            panelBuilder: (sc) => _panel(sc),
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(18.0),
+                topRight: Radius.circular(18.0)),
+            onPanelSlide: (double pos) => setState(() {
+              _fabHeight = pos * (_panelHeightOpen - _panelHeightClosed) +
+                  _initFabHeight;
+            }),
+          ),
+          // the fab
+          Positioned(
+            right: 20.0,
+            bottom: _fabHeight,
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.gps_fixed,
+                color: Theme.of(context).primaryColor,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Positioned(
+              top: 0,
+              child: ClipRRect(
+                  child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).padding.top,
+                        color: Colors.transparent,
+                      )))),
+          //the SlidingUpPanel Title
+          Positioned(
+            top: 52.0,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(24.0, 18.0, 24.0, 18.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24.0),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, .25), blurRadius: 16.0)
+                ],
+              ),
+              child: const Text(
+                "SlidingUpPanel Example",
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+// パネル側のWidget
+  Widget _panel(ScrollController sc) {
+    return MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            // controller: sc,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 30,
+                      height: 5,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(12.0))),
+                    ),
+                  ],
+                ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('（株）あああああああああ'),
+                    Text('東京都千代田区1-1-1')
+                  ],
+                ),
+                _defaultTab()
+                // _customTab()
+              ],
+            )));
+  }
+
+  Widget _customTab() {
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabController,
+          tabs: const [Tab(text: '1'), Tab(text: '2')],
+        ),
+        Center(
+          child: [
+            Text('first tab'),
+            Column(
+              children: [
+                Text('second tab'),
+                ...List.generate(10, (index) => Text('line: $index'))
+              ],
+            ),
+            Column(
+              children: [
+                Text('third tab'),
+                ...List.generate(20, (index) => Text('line: $index'))
+              ],
+            ),
+          ][_tabController!.index],
+        ),
+      ],
+    );
+  }
+
+  Widget _defaultTab() {
+    return const DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            tabs: [Tab(text: '1'), Tab(text: '2')],
+          ),
+          SizedBox(
+            height: 100,
+            child: TabBarView(
+              children: [
+                Center(
+                  child: Text('1'),
+                ),
+                Center(
+                  child: Text('2'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _button(String label, IconData icon, Color color) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+              color: color,
+              shape: BoxShape.circle,
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.15),
+                  blurRadius: 8.0,
+                )
+              ]),
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        Text(label),
+      ],
+    );
+  }
+
+// パネル裏のマップを表示するWidget
+  Widget _body() {
+    return FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(40.441589, -80.010948),
+          initialZoom: 13,
+          maxZoom: 15,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          )
+        ]);
   }
 }
